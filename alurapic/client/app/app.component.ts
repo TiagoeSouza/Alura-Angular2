82eee7e317ddf9c49fffb670a7345c9b652975c3
TiagoeSouza <tiago_e_souza@hotmail.com>
2018-03-26 23:19:40 -0300

    Finalizado

---
+++ alurapic/client/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { jsonpFactory } from '@angular/http/src/http_module';
+import { Json } from '@angular/core/src/facade/lang';
+
+@Component({
+    moduleId: module.id,
+    selector: 'app',
+    templateUrl: './app.component.html'
+
+})
+export class AppComponent {
+}
